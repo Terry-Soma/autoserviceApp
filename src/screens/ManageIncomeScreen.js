c722fa60b6f568148b05eb1639399d30e65c378c
Terry-Soma <jackhack116@yahoo.com>
2023-03-22 15:27:36 +0800

    some date function

--- src/screens/ManageIncomeScreen.js
+++ src/screens/ManageIncomeScreen.js
@@ -3,57 +3,61 @@ import axios from "axios";
 import React, { useCallback, useEffect, useState } from "react";
 import { Alert, FlatList, Pressable, SafeAreaView, Text, View } from "react-native";
 import thousandify from 'thousandify'
-import { restUrl } from "../../Constants";
+import { fontVar, restUrl } from "../../Constants";
+import { formatQueryToDate, formattedDate, formattedToday } from "../utils/date";
 import IncomeListItem from "./../components/IncomeList/IncomeListItem"
-
+import Spinner from './../components/Spinner'
 
 
 export default function ManageIncomeScreen({ navigate }) {
   const [filterOption, setFilterOption] = useState('today');
   const [incomeInfo, setIncomeInfo] = useState(null)
+  const [incomeLength, setIncomeLength] = useState(null)
+  const [amount, setAmount] = useState(null)
+  const [date, setDate] = useState(formattedToday())
+  // date 
   // today last3 last7 lastmonth last3month
   const searchOption = [{ title: "Өнөөдөр", option: "today" }, { title: "#3 хоног", option: "last3" }, { title: "#7 хоног", option: "last7" }, { title: "#1 cар", option: "lastmonth" }, { title: "#3 сар", option: "last3month" }];
-
-  // const someFunc = async (option) => {
-  //   try {
-  //     const res = await axios.get(restUrl + "/api/income" + "?sort=" + option);
-  //     console.log('res', res.data.data)
-  //     setIncomeInfo(res.data.data)
-  //   } catch (error) {
-  //     Alert.alert(error)
-  //     console.log('e', error)
-
-  //   }
-  // }
-  useEffect(() => {
-    fetchData();
-  }, [filterOption, fetchData])
   const fetchData = useCallback(async () => {
+    setDate(formatQueryToDate(filterOption))
     console.log('filteredOption', filterOption)
+    setIncomeInfo([])
     try {
       const res = await axios.get(restUrl + "/api/income" + "?sort=" + filterOption);
-      // console.log('res', res.data.data)
       setIncomeInfo(res.data.data)
+      setIncomeLength(res.data["length"])
+      setAmount(res.data.incomeResult)
     } catch (error) {
       Alert.alert(error)
       console.log('e', error)
     }
   }, [filterOption])
+  useEffect(() => {
+    console.log('bi ajillaaa ')
+
+    fetchData();
+  }, [filterOption, fetchData])
+
   return (
     <SafeAreaView style={{ marginHorizontal: 6, flex: 1 }}>
-      {/* niit orlogiig haruulah  
-      // flatlist?
-      
-      // 
-      
-      
-      */}
-      <View style={{ backgroundColor: "#2A7E73", width: "90%", height: 100, alignSelf: "center", marginVertical: 10 }}>
-        <Text style={{ color: "lightgrey" }}>03 / 21</Text>
-        <Text style={{ color: "white" }}>Нийт орлого
-          <Text>{thousandify(1000022)}₮</Text>
-        </Text>
-        <Text>10 ш худалдаа</Text>
+      {/* card like */}
+      {/* container */}
+      <View style={{ backgroundColor: "#2A7E73", marginVertical: 10, marginHorizontal: 12, paddingVertical: 8, borderRadius: 16, }}>
+        <Text style={{ color: "lightgrey", fontFamily: fontVar.Man, fontSize: 16, textAlign: "center" }}>{date}</Text>
+        <View style={{
+          paddingVertical: 12, marginHorizontal: 12, flexDirection: 'row',
+          flexWrap: 'wrap', justifyContent: "space-around"
+        }}>
+          <View style={{ alignItems: "center" }}>
+            <Text style={{ color: "white", fontSize: 14, fontFamily: fontVar.Mont.l, paddingLeft: 4 }}>Нийт орлого
+            </Text>
+            <Text style={{ color: "white", fontSize: 20, fontFamily: fontVar.Mont.b }}>{thousandify(amount)}₮</Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text style={{ color: "white", fontSize: 16, fontFamily: fontVar.Mont.b }}>Нийт {incomeLength} </Text>
+            <Text style={{ color: "white", fontSize: 14, fontFamily: fontVar.Mont.l }}>худалдаа</Text>
+          </View>
+        </View>
       </View>
       {/* шүүх */}
 
@@ -63,10 +67,10 @@ export default function ManageIncomeScreen({ navigate }) {
             horizontal
             showsHorizontalScrollIndicator={false}
             data={searchOption}
-            keyExtractor={(item, index) => index}
+            keyExtractor={(_, index) => index}
             renderItem={({ item }) => {
               return (
-                <View style={{ backgroundColor: "#F4B69C", marginHorizontal: 4, marginVertical: 2, borderRadius: 12, paddingVertical: 4, paddingHorizontal: 8, elevation: 4 }}>
+                <View style={[{ marginHorizontal: 4, marginVertical: 2, borderRadius: 12, paddingVertical: 4, paddingHorizontal: 8, elevation: 4 }, { backgroundColor: filterOption === item.option ? "#f49cae" : "#F4B69C" }]}>
                   <Pressable
                     style={({ pressed }) => [pressed && { opacity: 0.4 }]}
                     onPress={() => setFilterOption(item.option)}
@@ -77,15 +81,25 @@ export default function ManageIncomeScreen({ navigate }) {
               )
             }}
           />
+
+
         </View>
       </View>
 
-      <View style={{ paddingVertical: 12, backgroundColor: "green", flex: 1 }}>
-        <FlatList
-          data={incomeInfo}
-          keyExtractor={(income) => income.id}
-          renderItem={({ item }) => IncomeListItem(item)}
-        />
+      <View style={{
+        paddingVertical: 12,
+        backgroundColor: "#BEBFf5",
+        flex: 1
+      }}>
+        {incomeInfo && incomeInfo.length > 0 ? (
+          <FlatList
+            data={incomeInfo}
+            keyExtractor={(income) => income.id}
+            renderItem={({ item }) => IncomeListItem(item)}
+          />
+        ) : (<Spinner textStyle={{ color: "#333" }} />)
+        }
+
       </View>
 
 
--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,35 @@
-export function formattedDate(sell_date) {
-  let date = new Date(sell_date)
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} | ${date.getHours()}:${date.getMinutes()}м`;
+export function formattedDate(sell_date = null) {
 
+  let date = sell_date ? new Date(sell_date) : new Date()
+
+  return `${formattedToday(date)}| ${date.getHours()}:${date.getMinutes()}м`;
+
+}
+export function formattedToday(givenDate = null) {
+  let date = givenDate ? new Date(givenDate) : new Date()
+  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+}
+export function formatQueryToDate(query) {
+  // today last3 last7 lastmonth last3month
+  let originalDate = new Date();
+  let year = originalDate.getFullYear();
+  let month = originalDate.getMonth();
+  let date = originalDate.getDate()
+  switch (query) {
+    case "last7":
+      const last = originalDate.getDate() - 7;
+      break;
+      return `${originalDate.getFullYear()}/${originalDate.getMonth() + 1}/${date.getDate()}`;
+    case "last3":
+      return `${year}/${month + 1}/${date}`;
+      break;
+    case "lastmonth":
+      break;
+    case "last3month":
+      break;
+    default: formattedToday();
+      break;
+  }
+
+  return ``;
 }
